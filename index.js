352dea13587ee19069e7e7a63497a63f604c3009
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-14 16:12:34 -0700

    setting up basics

---
+++ index.js
@@ -0,0 +1,13 @@
+//create a way to check if the number the user enters is even or odd.
+
+const num = document.getElementById("numberBox");
+
+function checkNum() {
+    if (num % 2 === 0 ) {
+        console.log(`The number is even.`)
+    } else {
+        console.log(`The number is odd.`)
+    }
+}
+
+checkNum(80);
